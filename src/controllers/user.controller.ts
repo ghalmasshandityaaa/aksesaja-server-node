b50c40e278f3d322ca6081884144f1370c13e520
ghalmasshanditya <ghalmasshandityaaa@gmail.com>
2022-06-04 14:49:44 +0700

    implement new logic for auth

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,5 @@
 import { Response, Request } from 'express';
-
-/**
- * Where to import Services
- */
 import { UserService } from '../services/user.service';
-
-/**
- * Where to import Helpers
- */
-// const responseHelper = require('../helpers/response.helper');
-
-/**
- * Where to import Schema
- */
-// const ProductSchema = require('../schema/product.schema');
 
 export class UserController {
   static async getAllUsers(_: Request, res: Response) {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,26 +1,12 @@
-/**
- * Where to import Services
- */
-// const ProductService = require('../services/product.service');
-
-/**
- * Where to import Schema
- */
-// const ProductSchema = require('../schema/product.schema');
-
-/**
- * Where to import Models
- */
 import { SignIn, SignUp } from '~/interfaces/auth.interface';
 import { Users } from '../models/users';
 import { Connection } from '../config/db.config';
 import moment from 'moment';
 import { generateRandomNumber, textDecrypt } from '../helpers/helper';
-import { VerifyActivationCode } from '../interfaces/auth.interface';
 import { UserVerificationCode } from '../models/user-verification-code';
 
 export class AuthService {
-  constructor() { } // private readonly companyService: CompanyService,
+  constructor() { }
 
   static async signIn(params: SignIn) {
     try {
@@ -34,8 +20,6 @@ export class AuthService {
         throw Error('Sorry your password is wrong');
       } else if (!getUsers.isActive) {
         throw Error('Sorry your account is not activated');
-      } else if (!getUsers.password) {
-        return { result: 'Password is null', code: 204 };
       }
 
       return { result: getUsers, code: 200 };
@@ -45,27 +29,17 @@ export class AuthService {
     }
   }
 
-  static async signUp(params: SignUp) {
+  static async signUp(params: SignUp, email: string) {
     try {
-      const getUsers = await Users.createQueryBuilder('users')
-        .where('users.email = :email', { email: params.email })
-        .select('users.email')
-        .getOne();
-
-      if (getUsers) {
-        return { result: 'Email is registered', code: 409 };
-      }
-
       const datatemp = {
-        verificationCode: generateRandomNumber(6),
-        email: params.email,
-        retry: 0,
-        expiredDate: moment().add(1, 'days').format('YYYY-MM-DD HH:mm:ss'),
+        fullName: params.fullName,
+        email,
+        password: params.password,
         createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
         createdBy: 'API Auth SignUp',
       };
 
-      await Connection.createQueryBuilder().insert().into(UserVerificationCode).values(datatemp).execute();
+      await Connection.createQueryBuilder().insert().into(Users).values(datatemp).execute();
 
       return { result: null, code: 201 };
     } catch (e) {
@@ -74,15 +48,15 @@ export class AuthService {
     }
   }
 
-  static async verifyActivationCode(params: VerifyActivationCode) {
+  static async verifyActivationCode(email: string, activationCode: string) {
     try {
       const getUserVerificationCode = await UserVerificationCode.createQueryBuilder('code')
-        .where('code.email = :email', { email: params.email })
+        .where('code.email = :email', { email: email })
         .getOne();
 
       if (!getUserVerificationCode) {
         throw Error('Sorry your email is not registered');
-      } else if (getUserVerificationCode.verificationCode !== params.activationCode) {
+      } else if (getUserVerificationCode.verificationCode !== activationCode) {
         if (getUserVerificationCode.retry + 1 <= 3) {
           await Connection.createQueryBuilder()
             .update(UserVerificationCode)
@@ -91,7 +65,7 @@ export class AuthService {
               updatedAt: moment().format('YYYY-MM-DD HH:mm:ss'),
               updatedBy: 'API Auth VerifyActivationCode',
             })
-            .where('email = :email', { email: params.email })
+            .where('email = :email', { email: email })
             .execute();
 
           throw Error(`Sorry your activation code is wrong: ${getUserVerificationCode.retry + 1}x`);
@@ -99,32 +73,83 @@ export class AuthService {
           await Connection.createQueryBuilder()
             .delete()
             .from(UserVerificationCode)
-            .where('email = :email', { email: params.email })
+            .where('email = :email', { email: email })
             .execute();
 
           throw Error('Sorry you reached the maximum trial limit');
         }
-      } else if (getUserVerificationCode.verificationCode === params.activationCode) {
+      } else if (getUserVerificationCode.verificationCode === activationCode) {
         await Connection.createQueryBuilder()
           .insert()
           .into(Users)
           .values({
-            email: params.email,
+            email: email,
             isActive: true,
             createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
           })
-          .execute();
-
-        await Connection.createQueryBuilder()
-          .delete()
-          .from(UserVerificationCode)
-          .where('email = :email', { email: params.email })
           .execute();
       }
 
       return { result: 'Congratulations, your account has been successfully activated', code: 200 };
     } catch (e) {
       console.error({ service: 'AuthService.verifyActivationCode', message: e.message, stack: e.stack });
+      throw e;
+    }
+  }
+
+  static async checkAvailabilityEmail(email: string) {
+    try {
+      const checkEmail = await Users.createQueryBuilder('users')
+        .where('users.email = :email', { email })
+        .select('users.email')
+        .getOne();
+
+      if (checkEmail) return { result: 'Email is registered', code: 409 };
+
+      /** Generate activation code, Begin */
+      const datatemp = {
+        verificationCode: generateRandomNumber(6),
+        email,
+        retry: 0,
+        expiredDate: moment().add(1, 'days').format('YYYY-MM-DD HH:mm:ss'),
+        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        createdBy: 'API Auth SignUp',
+      };
+
+      await Connection.createQueryBuilder().insert().into(UserVerificationCode).values(datatemp).execute();
+      /** Generate activation code, End */
+
+      /** Send email, Begin */
+      await this.sendEmail(email, {}).then(
+        async (resp) => {
+          await this.setLogEMail(resp, {});
+        },
+        async (err) => {
+          await this.setLogEMail(err, {});
+        },
+      );
+      /** Send email, End */
+      return { result: null, code: 200 };
+    } catch (e) {
+      console.error({ service: 'AuthService.checkAvailabilityEmail', message: e.message, stack: e.stack });
+      throw e;
+    }
+  }
+
+  static async sendEmail(email: string, content = {}) {
+    try {
+      console.log(email, content);
+    } catch (e) {
+      console.error({ service: 'AuthService.sendEmail', message: e.message, stack: e.stack });
+      throw e;
+    }
+  }
+
+  static async setLogEMail(response: any, data: any) {
+    try {
+      console.log(response, data);
+    } catch (e) {
+      console.error({ service: 'AuthService.setLogEMail', message: e.message, stack: e.stack });
       throw e;
     }
   }
