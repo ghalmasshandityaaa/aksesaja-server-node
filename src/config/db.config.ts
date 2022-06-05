41932f9baad9f8641c4555fb10119d8de7762404
ipramudya <ipramudya.dev@gmail.com>
2022-06-05 12:37:08 +0700

    pull develop branch

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -8,6 +8,9 @@ dotenv.config();
 export const Connection = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/postgres',
+  username: 'eiljehfsyyerex',
+  password: 'a672bdf1632bab02d6914239a4d0e4dcd37c8f5b2d87994496b5ed2ad7712676',
+  database: 'dduvj2vb89fmvr',
   synchronize: false,
   logging: false,
   entities: [Users, UserVerificationCode],
