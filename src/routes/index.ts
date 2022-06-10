41d2edc3b23ff03b7e7a5b43abfc74eda2c69fa2
ghalmasshanditya <ghalmasshandityaaa@gmail.com>
2022-06-10 19:15:37 +0700

    remark

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -118,19 +118,11 @@ router.get('/myIp', async (req: Request, res: Response) => {
 
     const yy = req.get('host') === 'localhost:5001' ? 'transfer.greatdayhr.com' : req.get('host');
 
-    (async () => {
-      try {
-        const result = await ping.promise.probe(yy!, {
-          timeout: 10,
-          extra: ["-i", "2"],
-        });
+    ping.sys.probe(yy!, function (isAlive) {
+      var msg = isAlive ? 'host ' + yy + ' is alive' : 'host ' + yy + ' is dead';
+      console.log(msg);
+    });
 
-        console.log(result);
-      } catch (e) {
-        console.log(e);
-        res.json(e)
-      }
-    })();
     res.json(data)
   } catch (e) {
     res.json(e)
