dbfa38d9a7bfec3e01833160eaf1d885a990a0e0
ghalmasshanditya <ghalmasshandityaaa@gmail.com>
2022-06-11 13:26:58 +0700

    configure cors options

--- src/constants/server.constatnt.ts
+++ src/constants/server.constatnt.ts
@@ -3,5 +3,7 @@ import { CorsOptions } from "cors";
 export const CORS_OPTION: CorsOptions = {
   credentials: true,
   preflightContinue: true,
-  origin: ['http://localhost:3000', 'https://aksesaja-webapp-dev.vercel.app'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: 'Content-Type, Authorization, X-Requested-With, Accept, Origin, Cache-Control, Application/json, X-HTTP-Method-Override',
+  origin: ['http://localhost:3000', 'https://aksesaja-webapp-dev.vercel.app', 'https://aksesaja.vercel.app'],
 }
