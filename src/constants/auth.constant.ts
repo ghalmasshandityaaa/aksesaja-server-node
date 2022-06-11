6da8e395b886a0e104de08f3aaa7e211be7408e5
ghalmasshanditya <ghalmasshandityaaa@gmail.com>
2022-06-11 14:44:45 +0700

    delete httponly

--- src/constants/auth.constant.ts
+++ src/constants/auth.constant.ts
@@ -4,4 +4,4 @@ const APP_MODE = Config.get('APP_MODE');
 
 export const SECURE_COOKIES = APP_MODE !== 'localhost';
 export const SAME_SITE_COOKIES = SECURE_COOKIES ? 'none' : 'lax';
-export const COOKIES_OPTIONS: CookieOptions = { httpOnly: true, secure: SECURE_COOKIES, sameSite: SAME_SITE_COOKIES };
+export const COOKIES_OPTIONS: CookieOptions = { secure: SECURE_COOKIES, sameSite: SAME_SITE_COOKIES };
